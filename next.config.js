0a05ad5018f47705bb598c852b10a4598f6239a6
shabaraba <work.shabaraba@gmail.com>
2022-02-28 22:54:26 +0900

    - fix webpacks for jsdom

--- next.config.js
+++ next.config.js
@@ -1,1 +1,22 @@
-module.exports = { target: 'serverless' }
+module.exports = {
+  target: 'serverless',
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (isServer) {
+      config.plugins.push(new webpack.IgnorePlugin(
+        {
+          resourceRegExp: /canvas/,
+          contextRegExp: /jsdom$/,
+        }))
+      // config.module.rules.push({
+      //   test: /\.node$/,
+      //   __dirname: false,
+      //   use: [
+      //     {
+      //       loader: "node-loader",
+      //     },
+      //   ],
+      // })
+    }
+    return config
+  }
+}
