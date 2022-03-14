95d192b5b8ab3ff15372695af4c807999f95c4c4
shabaraba <work.shabaraba@gmail.com>
2022-03-14 09:25:31 +0900

    - add link tag to access rss.xml and atom.xml

--- next-seo.config.js
+++ next-seo.config.js
@@ -14,5 +14,20 @@ export default {
     site: '@shaba_raba',
     cardType: 'summary',
   },
+  additionalLinkTags: [
+    {
+      rel: 'alternate',
+      type: 'application/rss+xml',
+      title: "RSS2.0",
+      href: 'https://blog.from-garage.com/rss/feed.xml',
+    },
+    {
+      rel: 'alternate',
+      type: 'application/atom+xml',
+      title: "Atom",
+      href: 'https://blog.from-garage.com/rss/atom.xml',
+    },
+
+  ],
   description: siteDescription,
 };
