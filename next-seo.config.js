7799b7304cb009df5b0d85baf114e80b1b51bdd0
shaba <53943902+shabaraba@users.noreply.github.com>
2023-02-14 17:14:26 +0900

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,6 @@
 export const siteTitle = 'Coffee Break Point'
 export const siteDescription = 'コーヒー休憩にちょうどよい技術よみものを目指して'
-export const siteUrl = 'https://blog.from-garage.com'
+export const siteUrl = 'https://blog.shaba.dev'
 
 export default {
   defaultTitle: siteTitle,
@@ -25,13 +25,13 @@ export default {
       rel: 'alternate',
       type: 'application/rss+xml',
       title: "RSS2.0",
-      href: 'https://blog.from-garage.com/rss/feed.xml',
+      href: 'https://blog.shaba.dev/rss/feed.xml',
     },
     {
       rel: 'alternate',
       type: 'application/atom+xml',
       title: "Atom",
-      href: 'https://blog.from-garage.com/rss/atom.xml',
+      href: 'https://blog.shaba.dev/rss/atom.xml',
     },
 
   ],
